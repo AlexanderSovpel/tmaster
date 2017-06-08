5afa7e2f55023671bdd4e5a9527dd1f9a9fb7c22
Alexander <alexander.sovpel@gmail.com>
2017-06-08 16:56:57 +0300

    fixing...

--- database/migrations/2017_06_08_101652_add_foreign_keys.php
+++ database/migrations/2017_06_08_101652_add_foreign_keys.php
@@ -13,41 +13,41 @@ class AddForeignKeys extends Migration
      */
     public function up()
     {
-      if (Schema::hasTable('tournaments')) {
-        Schema::table('tournaments', function (Blueprint $table) {
-          $table->foreign('handicap_id')->references('id')->on('handicaps')->onDelete('cascade');
-          $table->foreign('qualification_id')->references('id')->on('qualifications')->onDelete('cascade');
-          $table->foreign('roundrobin_id')->references('id')->on('roundrobins')->onDelete('cascade');
-          $table->foreign('contact_id')->references('id')->on('users');
-        });
-      }
-
-      if (Schema::hasTable('squads')) {
-        Schema::table('squads', function (Blueprint $table) {
-          $table->foreign('tournament_id')->references('id')->on('tournaments');
-        });
-      }
-
-      if (Schema::hasTable('squad_players')) {
-        Schema::table('squad_players', function (Blueprint $table) {
-          $table->foreign('squad_id')->references('id')->on('squads');
-          $table->foreign('player_id')->references('id')->on('users');
-        });
-      }
-
-      if (Schema::hasTable('games')) {
-        Schema::table('games', function (Blueprint $table) {
-          $table->foreign('player_id')->references('id')->on('users');
-          $table->foreign('tournament_id')->references('id')->on('tournaments');
-        });
-      }
-
-      if (Schema::hasTable('results')) {
-        Schema::table('results', function (Blueprint $table) {
-          $table->foreign('tournament_id')->references('id')->on('tournaments');
-          $table->foreign('player_id')->references('id')->on('users');
-        });
-      }
+//      if (Schema::hasTable('tournaments')) {
+//        Schema::table('tournaments', function (Blueprint $table) {
+//          $table->foreign('handicap_id')->references('id')->on('handicaps')->onDelete('cascade');
+//          $table->foreign('qualification_id')->references('id')->on('qualifications')->onDelete('cascade');
+//          $table->foreign('roundrobin_id')->references('id')->on('roundrobins')->onDelete('cascade');
+//          $table->foreign('contact_id')->references('id')->on('users');
+//        });
+//      }
+//
+//      if (Schema::hasTable('squads')) {
+//        Schema::table('squads', function (Blueprint $table) {
+//          $table->foreign('tournament_id')->references('id')->on('tournaments');
+//        });
+//      }
+//
+//      if (Schema::hasTable('squad_players')) {
+//        Schema::table('squad_players', function (Blueprint $table) {
+//          $table->foreign('squad_id')->references('id')->on('squads');
+//          $table->foreign('player_id')->references('id')->on('users');
+//        });
+//      }
+//
+//      if (Schema::hasTable('games')) {
+//        Schema::table('games', function (Blueprint $table) {
+//          $table->foreign('player_id')->references('id')->on('users');
+//          $table->foreign('tournament_id')->references('id')->on('tournaments');
+//        });
+//      }
+//
+//      if (Schema::hasTable('results')) {
+//        Schema::table('results', function (Blueprint $table) {
+//          $table->foreign('tournament_id')->references('id')->on('tournaments');
+//          $table->foreign('player_id')->references('id')->on('users');
+//        });
+//      }
     }
 
     /**
@@ -57,40 +57,40 @@ class AddForeignKeys extends Migration
      */
     public function down()
     {
-      if (Schema::hasTable('tournaments')) {
-        Schema::table('tournaments', function (Blueprint $table) {
-          $table->dropForeign('tournaments_handicap_id_foreign');
-          $table->dropForeign('tournaments_qualification_id_foreign');
-          $table->dropForeign('tournaments_roundrobin_id_foreign');
-          $table->dropForeign('tournaments_contact_id_foreign');
-        });
-      }
-
-      if (Schema::hasTable('squads')) {
-        Schema::table('squads', function (Blueprint $table) {
-          $table->dropForeign('squads_tournament_id_foreign');
-        });
-      }
-
-      if (Schema::hasTable('squad_players')) {
-        Schema::table('squad_players', function (Blueprint $table) {
-          $table->dropForeign('squad_players_squad_id_foreign');
-          $table->dropForeign('squad_players_player_id_foreign');
-        });
-      }
-
-      if (Schema::hasTable('games')) {
-        Schema::table('games', function (Blueprint $table) {
-          $table->dropForeign('games_player_id_foreign');
-          $table->dropForeign('games_tournament_id_foreign');
-        });
-      }
-
-      if (Schema::hasTable('results')) {
-        Schema::table('results', function (Blueprint $table) {
-          $table->dropForeign('results_tournament_id_foreign');
-          $table->dropForeign('results_player_id_id_foreign');
-        });
-      }
+//      if (Schema::hasTable('tournaments')) {
+//        Schema::table('tournaments', function (Blueprint $table) {
+//          $table->dropForeign('tournaments_handicap_id_foreign');
+//          $table->dropForeign('tournaments_qualification_id_foreign');
+//          $table->dropForeign('tournaments_roundrobin_id_foreign');
+//          $table->dropForeign('tournaments_contact_id_foreign');
+//        });
+//      }
+//
+//      if (Schema::hasTable('squads')) {
+//        Schema::table('squads', function (Blueprint $table) {
+//          $table->dropForeign('squads_tournament_id_foreign');
+//        });
+//      }
+//
+//      if (Schema::hasTable('squad_players')) {
+//        Schema::table('squad_players', function (Blueprint $table) {
+//          $table->dropForeign('squad_players_squad_id_foreign');
+//          $table->dropForeign('squad_players_player_id_foreign');
+//        });
+//      }
+//
+//      if (Schema::hasTable('games')) {
+//        Schema::table('games', function (Blueprint $table) {
+//          $table->dropForeign('games_player_id_foreign');
+//          $table->dropForeign('games_tournament_id_foreign');
+//        });
+//      }
+//
+//      if (Schema::hasTable('results')) {
+//        Schema::table('results', function (Blueprint $table) {
+//          $table->dropForeign('results_tournament_id_foreign');
+//          $table->dropForeign('results_player_id_id_foreign');
+//        });
+//      }
     }
 }
