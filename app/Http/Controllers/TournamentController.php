b415b1ed0e9cc31ff26abe247f94db2487360927
Alexander <alexander.sovpel@gmail.com>
2017-06-08 16:26:55 +0300

    fixing...

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -375,17 +375,17 @@ class TournamentController extends Controller
 
     public function roundRobinResults(Request $request, $tournamentId)
     {
-        echo "hello<br>";
-        echo gettype($request->input('players'))."<br>";
-        echo $request->input('players')."<br>";
+//        echo "hello<br>";
+//        echo gettype($request->input('players'))."<br>";
+//        echo $request->input('players')."<br>";
         $players = json_decode($request->input('players'));
-        echo json_last_error()."<br>";
-        echo gettype($players)."<br>";
-        echo count($players)."<br>";
-        var_dump($players)."<br>";
+//        echo json_last_error()."<br>";
+//        echo gettype($players)."<br>";
+//        echo count($players)."<br>";
+//        var_dump($players)."<br>";
         foreach ($players as $key => $player) {
-            echo gettype($player)."<br>";
-            echo $key."<br>";
+//            echo gettype($player)."<br>";
+            echo $key.": ".$player."<br>";
 //            echo "$key: $player<br>";
         }
 //        $playedRoundRobinGames = array();
