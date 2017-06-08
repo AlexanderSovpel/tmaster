a1edde2bc06b062b51f2c3f79e4eb187edfd4c29
Alexander <alexander.sovpel@gmail.com>
2017-06-08 12:58:00 +0300

    Testing Debugbar.

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -25,7 +25,7 @@ class TournamentController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
@@ -36,7 +36,7 @@ class TournamentController extends Controller
     public function index()
     {
       return 'Hello!';
-      Debugbar::info(User::all());
+      // Debugbar::info(User::all());
       // return User::all();
 
         // $tournaments = Tournament::all()->sortByDesc('id');
