ae494ab50b9942d52793b3315f281966f5c2ce11
Alexander <alexander.sovpel@gmail.com>
2017-06-08 15:29:47 +0300

    fixind

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -375,13 +375,13 @@ class TournamentController extends Controller
 
     public function roundRobinResults(Request $request, $tournamentId)
     {
-
-        $players = json_decode($request->input('players'));
-        $playedRoundRobinGames = array();
-        $playersResults = array();
-        $qualificationResults = array();
-        foreach ($players as $player) {
-            echo $player."<br>";
+            return "hello";
+//        $players = json_decode($request->input('players'));
+//        $playedRoundRobinGames = array();
+//        $playersResults = array();
+//        $qualificationResults = array();
+//        foreach ($players as $player) {
+//            echo $player."<br>";
 
 //            $roundRobinGames = Game::where('player_id', $player->id)
 //                ->where('tournament_id', $tournamentId)
@@ -413,7 +413,7 @@ class TournamentController extends Controller
 //            $roundRobinResult->save();
 //            $qualificationResults[$player->id] = $qualificationResult;
 //            $playersResults[$player->id] = $roundRobinResult;
-        }
+//        }
 //
 ////        echo gettype($players);
 ////        $players = array_values($players);
