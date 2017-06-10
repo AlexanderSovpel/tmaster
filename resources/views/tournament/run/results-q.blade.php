efe409700eeab20ecc90974780ef5da3d0a8a666
Alexander <alexander.sovpel@gmail.com>
2017-06-10 04:35:04 +0300

    Fixing reentries.

--- resources/views/tournament/run/results-q.blade.php
+++ resources/views/tournament/run/results-q.blade.php
@@ -2,7 +2,7 @@
 
 @section('process')
 <h2>Квалификация - Результаты</h2>
-<form action="/{{$tournament->id}}/run/rr/conf/" method="post">
+<form action="/{{$tournament->id}}/run/rr/conf/" method="get">
     {{ csrf_field() }}
     <input type="hidden" name="players" value="{{json_encode($qPlayers)}}">
     @include('partial.qualification-results')
