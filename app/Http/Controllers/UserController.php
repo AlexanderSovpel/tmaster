11b85e6c5bc3baa3e94228f2ec582def957e22e4
Alexander <alexander.sovpel@gmail.com>
2017-06-15 01:15:26 +0300

    Setting avatar uploading.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,9 +13,9 @@ class UserController extends Controller
 {
   public function __construct()
   {
-      $this->middleware('auth');
+      // $this->middleware('auth');
   }
-  
+
     public function showAccount()
     {
         $user = Auth::user();
