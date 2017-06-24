871cf6a8872cd6741768212efa1dae2dbe483d84
Alexander <alexander.sovpel@gmail.com>
2017-06-24 18:33:24 +0300

    Testing middleware.

--- resources/views/tournament/players.blade.php
+++ resources/views/tournament/players.blade.php
@@ -12,7 +12,7 @@
                 <ol class="players-list">
                     @foreach($squad->players as $player)
                         <li>{{$player->surname}} {{$player->name}}</li>
-                        @if ($player->id == \Illuminate\Support\Facades\Auth::id() && !$tournament->finished)
+                        @if (\Illuminate\Support\Facades\Auth::check() && $player->id == \Illuminate\Support\Facades\Auth::id() && !$tournament->finished)
                             <form action='{{url("$tournament->id/removeApplication")}}' method="post">
                                 {{ csrf_field() }}
                                 <input type="hidden" name="currentSquad" value="{{$squad->id}}">
