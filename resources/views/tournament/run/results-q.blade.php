18cb283273813f6d9fdf7aed8a47224f202ba81a
Alexander <alexander.sovpel@gmail.com>
2017-06-30 11:31:32 +0300

    Fixing last button in 'no-final' tournaments.

--- resources/views/tournament/run/results-q.blade.php
+++ resources/views/tournament/run/results-q.blade.php
@@ -5,6 +5,10 @@
 <form action="/{{$tournament->id}}/run/rr/conf/" method="get">
     {{ csrf_field() }}
     @include('partial.qualification-results')
+    @if ($tournament->roundRobin->players)
     <button type="submit" class="btn">начать финал</button>
+    @else
+    <button type="submit" class="btn">завершить соревнование</button>
+    @endif
 </form>
 @endsection
