70e884281567f9c0af52310ca106826f1c6aec65
Alexander <alexander.sovpel@gmail.com>
2017-07-04 15:58:33 +0300

    Исправлены баги внешнего вида.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-        @if(\Illuminate\Support\Facades\Auth::check() && $user->is_admin)
-            <a href="/newTournament" class="add-tournament-btn">добавить соревнование</a>
-        @endif
-        @foreach($tournaments as $tournament)
-            @include('partial.tournament')
-        @endforeach
+    @if(\Illuminate\Support\Facades\Auth::check() && $user->is_admin)
+        <a href="/newTournament" class="add-tournament-btn">добавить соревнование</a>
+    @endif
+    @foreach($tournaments as $tournament)
+        @include('partial.tournament')
+    @endforeach
+    <div class="pagination-center">
+      {{$tournaments->links()}}
+    </div>
 @endsection
